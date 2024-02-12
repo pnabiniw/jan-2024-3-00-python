2436020884adc2164112760a35d7d2d02a7665a2
npaudyal321 <naween321@gmail.com>
2024-02-12 16:24:16 +0545

    file crud start

---
+++ day21/test1.py
@@ -0,0 +1,12 @@
+def func1():
+    print("Hello World")
+
+def addition(a, b):
+    return a + b
+
+print(__name__)
+if __name__ == '__main__':
+
+    func1()
+    print(addition(3, 2))
+
---
+++ day21/test2.py
@@ -0,0 +1,9 @@
+from test1 import addition
+
+def func2():
+    print("Another Message")
+
+
+
+print(addition(5, 5))
+func2()
